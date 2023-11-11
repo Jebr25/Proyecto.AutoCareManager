df50455824e7b32b69190a1d2540a2b3f866aa36
wilfredo <wilfredo.ramos@tecsup.edu.pe>
2023-11-11 18:28:07 -0500

    EmpleadoRepository

---
+++ Proyecto.AutoCareManager.DOMAIN/Core/Interfaces/IEmpleadoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Proyecto.AutoCareManager.DOMAIN.Core.Entities;
+
+namespace Proyecto.AutoCareManager.DOMAIN.Core.Interfaces
+{
+    public interface IEmpleadoRepository
+    {
+        Task<bool> Delete(int CodEmpleado);
+        Task<IEnumerable<TbEmpleado>> GetEmpleados();
+        Task<TbEmpleado> GetEmpleados(int CodEmpleado);
+        Task<bool> Insert(TbEmpleado empleado);
+        Task<bool> Update(TbEmpleado empleado);
+    }
+}
