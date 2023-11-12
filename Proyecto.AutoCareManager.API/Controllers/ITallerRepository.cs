8229652052ff8ae0b330b3b9f6efdeb93f60b8d0
wilfredo <wilfredo.ramos@tecsup.edu.pe>
2023-11-11 19:04:24 -0500

    TALLER REPOSITORY

---
+++ Proyecto.AutoCareManager.API/Controllers/ITallerRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Proyecto.AutoCareManager.API.Controllers
+{
+    internal interface ITallerRepository
+    {
+    }
+}
---
+++ Proyecto.AutoCareManager.DOMAIN/Core/Interfaces/ITallerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Proyecto.AutoCareManager.DOMAIN.Core.Entities;
+
+namespace Proyecto.AutoCareManager.DOMAIN.Core.Interfaces
+{
+    internal interface ITallerRepository
+    {
+        Task<bool> Delete(int CodTaller);
+        Task<TbTaller> GetTaller(int CodTaller);
+        Task<IEnumerable<TbTaller>> GetTalleres();
+        Task<bool> Insert(TbTaller taller);
+        Task<bool> Update(TbTaller taller);
+    }
+}
